48cddf77351a34f157a8462837f9154834e6eb4b
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2025-06-05 15:15:36 +0200

    Add helper to connect to SQLite database

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 from flask_session import Session
 
 
-# Create Flask app instance with relative config folder
+# Create Flask app instance with instance-relative config folder
 app = Flask(__name__, instance_relative_config=True)
 
 # Load configuration from instance/config.py
@@ -14,16 +14,28 @@ app.config.from_pyfile("config.py")
 Session(app)
 
 
-# Define homepage route
+# Helper functions
+def get_db_connection():
+    """
+    Create and return  a connection to the SQLite database.
+
+    Rows will be returned as dictionaries for easier access by column
+    name.
+    """
+    conn = sqlite3.connect(app.config["DATABASE"])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+# Routes
 @app.route("/")
 def index():
     """
     Render the home page.
     """
-    
     return render_template("index.html")
 
 
-# Run the app only if this file is executed directly
+# Run the app only when this file is executed directly
 if __name__ == "__main__":
     app.run()
