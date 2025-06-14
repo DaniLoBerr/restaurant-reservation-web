90cd7f940d8e1b419579c5a876810f4accca8a0a
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2025-06-14 15:42:06 +0200

    feat: add auth blueprint and register in factory
    Create 'auth.py' to define the authentication blueprint.
    Register it in the application factory function in
    '__init__.py'.

--- restaurant/__init__.py
+++ restaurant/__init__.py
@@ -28,7 +28,10 @@ def create_app(test_config=None):
 
     # Register database initialization with the app instance
     from . import db
-    
     db.init_app(app)
+
+    # Register auth blueprint
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
