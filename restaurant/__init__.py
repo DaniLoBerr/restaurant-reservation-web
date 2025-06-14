80b6521a2df4eb6674c1e4fc3d06cf975fe24818
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2025-06-14 15:19:29 +0200

    refactor: update comments in __init__.py

--- restaurant/__init__.py
+++ restaurant/__init__.py
@@ -4,10 +4,12 @@ from flask import Flask
 
 
 def create_app(test_config=None):
-    """Create and configure the Flask application."""
+    """Create and configure the Flask app using the factory pattern."""
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
+        # A default secret key that should be overriden by instance config
         SECRET_KEY="dev",
+        # Store the database in the instance folder
         DATABASE=os.path.join(app.instance_path, "restaurant.sqlite"),
     )
 
@@ -26,6 +28,7 @@ def create_app(test_config=None):
 
     # Register database initialization with the app instance
     from . import db
+    
     db.init_app(app)
 
     return app
