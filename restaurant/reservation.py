efba6f213b1b0b10958cfa190303e340613e9b86
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2025-08-15 11:41:54 +0100

    feat(reservation-bp): Implement the index view

--- restaurant/reservation.py
+++ restaurant/reservation.py
@@ -7,3 +7,20 @@ from restaurant.auth import login_required
 from restaurant.db import get_db
 
 bp = Blueprint("reservation", __name__)
+
+
+@bp.route("/")
+def index():
+    """Display the main page of the website.
+    
+    Lists all booked reservations from all users, ordered by date in
+    ascending order.
+    """
+    db = get_db()
+    reservations = db.execute(
+        "SELECT users.username, date, party_size " \
+        "FROM reservations " \
+        "JOIN users ON users.id = reservations.user_id " \
+        "ORDER BY date ASC"
+    ).fetchall()
+    return render_template("reservation/index.html", reservations=reservations)
