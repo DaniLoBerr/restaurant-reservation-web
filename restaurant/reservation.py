b4095412d3b3aa16c9c1c0b36a59de70627aac54
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2025-08-27 08:41:20 +0100

    feat(deploy): prepare project for deployment
    - Add required dependencies to pyproject.toml.
    - Add matching pinned versions to requirements.txt.
    - Remove unused dependencies that were no longer needed.
    - Clean up unnecessary imports from reservation.py and db.py.

--- restaurant/reservation.py
+++ restaurant/reservation.py
@@ -1,7 +1,6 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
-from werkzeug.exceptions import abort
 
 from restaurant.auth import login_required
 from restaurant.db import get_db
